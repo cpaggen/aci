0beac5c1fd5acf1505943c9ce11191d5b6ff0553
Christopher Paggen (cpaggen) <cpaggen@cisco.com>
2017-03-08 18:20:52 +0100

    password in env variable

--- events.py
+++ events.py
@@ -9,22 +9,31 @@ from acitoolkit.acitoolkit import *
 from websocket import *
 import email.utils
 from email.mime.text import MIMEText
+import os
+import sys
+
+    
+try:  
+    password = os.environ["ACI-password"]
+except KeyError:
+    print "Please set the ACI-password environment variable"
+    sys.exit(1)
 
 def sendMail(mailFrom,mailTo,subject,text,server):
     msg = MIMEText(text)
     msg['To'] = email.utils.formataddr(('Recipient', mailTo))
     msg['From'] = email.utils.formataddr(('ACI in the lab', mailFrom))
-    msg['Subject'] = 'ACI event!'
+    msg['Subject'] = 'ACI event for tenant TEN-01!'
     server = smtplib.SMTP(server)
     server.sendmail(mailFrom,mailTo,msg.as_string())
     server.quit()
 
-session = Session("https://apic_ip", "user", "password")
+session = Session("https://10.50.139.156", "admin", password)
 session.login()
 
 print session.token
 
-sub_url = '/api/node/class/fvTenant.json?query-target-filter=and(eq(fvTenant.name,"foobar"))&query-target=subtree&subscription=yes'
+sub_url = '/api/node/class/fvTenant.json?query-target-filter=and(eq(fvTenant.name,"TEN-01"))&query-target=subtree&subscription=yes'
 
 session.subscribe(sub_url)
 
@@ -33,4 +42,4 @@ while True:
         print "--- New event ---"
         event = session.get_event(sub_url)
         print event
-	sendMail("aci@mylab.example.org","admin@example.org","ACI event!",json.dumps(event),"mail.example.org")
+	sendMail("aci@cpoclon.cisco.com","aleza@cisco.com","ACI event!",json.dumps(event),"mail.cisco.com")
